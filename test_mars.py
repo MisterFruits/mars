b9c45bafa5c737dee1c5e2c1842f388b0b2e0a99
Victor <vic.toad.tor@free.fr>
2016-11-23 01:53:51 +0100

    NEW tests

---
+++ test_mars.py
@@ -0,0 +1,13 @@
+from mars import get_matrix
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+def test_get_matrix():
+    index = ['GEN1', 'GEN2']
+    d = {'LIB1' : pd.Series([1, 0], index=index),
+         'LIB2' : pd.Series([0, 25], index=index),
+         'LIB3' : pd.Series([0, 3], index=index)}
+
+    df = pd.DataFrame(d)
+
+    assert_frame_equal(df, get_matrix("mock.txt"))
