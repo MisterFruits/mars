c301abe8fdd9afeeea6d69c91ee72f2b3b4816d0
Victor <vic.toad.tor@free.fr>
2016-11-23 02:07:47 +0100

    CLEAN naming

--- mars.py
+++ mars.py
@@ -5,7 +5,7 @@ Minimalist program to exploit mars matrixes
 import argparse
 import pandas as pd
 
-def get_matrix(filepath_or_buffer):
+def read_matrix(filepath_or_buffer):
     index = ['GEN1', 'GEN2']
     d = {'LIB1' : pd.Series([1, 0], index=index),
          'LIB2' : pd.Series([0, 25], index=index),
@@ -18,7 +18,7 @@ def main():
     ap = argparse.ArgumentParser(description=__doc__)
     ap.add_argument('matrix', help='path to the matrix file')
 
-    args = ap.parse()
+    args = ap.parse_args()
 
     print(read_matrix(args.matrix))
 
--- test_mars.py
+++ test_mars.py
@@ -1,15 +1,15 @@
-from mars import get_matrix
+from mars import read_matrix
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
 
-def test_get_matrix():
+def test_read_matrix():
     index = ['GEN1', 'GEN2']
     d = {'LIB1' : pd.Series([1, 0], index=index),
          'LIB2' : pd.Series([0, 25], index=index),
          'LIB3' : pd.Series([0, 3], index=index)}
 
     df = pd.DataFrame(d)
-    assert_frame_equal(df, get_matrix("mock.txt"))
+    assert_frame_equal(df, read_matrix("mock.txt"))
 
     index = ['GEN45', 'GEN2']
     d = {'LIB1' : pd.Series([1, 0], index=index),
@@ -17,7 +17,7 @@ def test_get_matrix():
          'LIB3' : pd.Series([0, 3], index=index)}
 
     df = pd.DataFrame(d)
-    assert not df.equals(get_matrix("mock.txt"))
+    assert not df.equals(read_matrix("mock.txt"))
 
     index = ['GEN1', 'GEN2']
     d = {'LIB1' : pd.Series([1, 0], index=index),
@@ -25,7 +25,7 @@ def test_get_matrix():
          'LIB3' : pd.Series([0, 3], index=index)}
 
     df = pd.DataFrame(d)
-    assert not df.equals(get_matrix("mock.txt"))
+    assert not df.equals(read_matrix("mock.txt"))
 
     index = ['GEN1', 'GEN2']
     d = {'LIB1' : pd.Series([1, 0], index=index),
@@ -33,4 +33,4 @@ def test_get_matrix():
          'LIB3' : pd.Series([0, 3], index=index)}
 
     df = pd.DataFrame(d)
-    assert not df.equals(get_matrix("mock.txt"))
+    assert not df.equals(read_matrix("mock.txt"))
